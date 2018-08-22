10ca306941b870564e55e162358539b4e75d7e9f
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2018-08-22 23:07:29 +0600

    lb: add test
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- lb/lb_test.go
+++ lb/lb_test.go
@@ -1,6 +1,7 @@
 package lb
 
 import (
+	"math/rand"
 	"testing"
 )
 
@@ -30,6 +31,48 @@ func TestLB1(t *testing.T) {
 	})
 
 	if <-ch != 2 {
+		t.FailNow()
+	}
+}
+
+func TestLB2(t *testing.T) {
+	var sum, sum1 int
+	workers := 5
+	ch := make(chan int, workers)
+	// test data
+	data := make([]int, 100)
+	for i, _ := range data {
+		data[i] = rand.Int()
+	}
+
+	// direct sum
+	for _, i := range data {
+		sum += i
+	}
+
+	// do it with LoadBalancer
+	getF := func(pos, off int) func() {
+		return func() {
+			var s int
+			for _, i := range data[pos : pos+off] {
+				s += i
+			}
+			ch <- s
+		}
+	}
+
+	lb := New(Config{
+		Workers:    workers,
+		Mode:       RoundRobin,
+		BufferSize: 32})
+	for i := 0; i < workers; i++ {
+		lb.Do(getF(20*i, 20))
+	}
+	for i := 0; i < workers; i++ {
+		sum1 += <-ch
+	}
+
+	if sum != sum1 {
 		t.FailNow()
 	}
 }
