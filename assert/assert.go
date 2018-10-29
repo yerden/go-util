227136487cec9ca3009469cf304c6e7f4835de8f
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2018-10-29 13:35:49 +0600

    assert: add default error comment
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- assert/assert.go
+++ assert/assert.go
@@ -20,39 +20,63 @@ type Assert interface {
 	NotTrue(bool, ...string)
 }
 
+var (
+	nilStr = func(x interface{}) string {
+		return fmt.Sprintf("%v != nil", x)
+	}
+	notNilStr = func(x interface{}) string {
+		return fmt.Sprintf("%v == nil", x)
+	}
+	equalStr = func(x, y interface{}) string {
+		return fmt.Sprintf("%v != %v", x, y)
+	}
+	notEqualStr = func(x, y interface{}) string {
+		return fmt.Sprintf("%v != %v", x, y)
+	}
+	isTrue = func(x bool) string {
+		return fmt.Sprintf("%v != %v", x, true)
+	}
+	isNotTrue = func(x bool) string {
+		return fmt.Sprintf("%v != %v", x, false)
+	}
+)
+
 func New(t *testing.T) Assert {
 	return &assert{t: t}
 }
 
-func (a *assert) checkTrue(x bool, s []string) {
+func (a *assert) checkTrue(x bool, dfl string, s []string) {
 	_, fileName, fileLine, ok := runtime.Caller(2)
 	if !ok || x {
 		return
+	}
+	if len(s) == 0 {
+		s = append(s, dfl)
 	}
 	fmt.Printf("%s:%d: %s\n", fileName, fileLine, s)
 	a.t.FailNow()
 }
 
 func (a *assert) True(x bool, s ...string) {
-	a.checkTrue(x, s)
+	a.checkTrue(x, isTrue(x), s)
 }
 
 func (a *assert) NotTrue(x bool, s ...string) {
-	a.checkTrue(!x, s)
+	a.checkTrue(!x, isNotTrue(x), s)
 }
 
 func (a *assert) Equal(x interface{}, y interface{}, s ...string) {
-	a.checkTrue(reflect.DeepEqual(x, y), s)
+	a.checkTrue(reflect.DeepEqual(x, y), equalStr(x, y), s)
 }
 
 func (a *assert) NotEqual(x interface{}, y interface{}, s ...string) {
-	a.checkTrue(!reflect.DeepEqual(x, y), s)
+	a.checkTrue(!reflect.DeepEqual(x, y), notEqualStr(x, y), s)
 }
 
 func (a *assert) NotNil(x interface{}, s ...string) {
-	a.checkTrue(x != nil, s)
+	a.checkTrue(x != nil, notNilStr(x), s)
 }
 
 func (a *assert) Nil(x interface{}, s ...string) {
-	a.checkTrue(x == nil, s)
+	a.checkTrue(x == nil, nilStr(x), s)
 }
